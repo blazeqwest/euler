e8f6cc1db0fd7c1ddab552dec8d8e2187b83a751
blazeqwest <blazeqwest@ya.ru>
2013-03-15 13:43:30 +0200

    Update primes.js
    added few comments and hardcoded initial input

--- primes.js
+++ primes.js
@@ -1,4 +1,8 @@
-var num = prompt("number");
+//* Problem statement:
+//* The prime factors of 13195 are 5, 7, 13 and 29.
+//* What is the largest prime factor of the number 600851475143 ?
+
+var num = 600851475143;
 var primes = [];
 var j = 0;
 for (var i = 2; i < num; i++){
